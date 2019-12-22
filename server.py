c0310a54f84a17daf4a0e6cf87275ae30d80affa
MichaelInAction <michael.read.75@gmail.com>
2019-12-22 02:32:17 -0500

    API test

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask_jsonpify import jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+class Tweet(Resource):
+    def get(self):
+        return jsonify({'employees': {'message': 'hi'}})
+
+api.add_resource(Employees, '/tweet')
+
+if __name__ == '__main__':
+    app.run(port='5002')
